7feb15215fad769ee434eff028d060e56f440e49
Samarth Shah <samarthshah3140@gmail.com>
2021-03-17 14:53:41 -0700

    v1.0

---
+++ js/weatherapp.js
@@ -0,0 +1,61 @@
+const imageElement = document.querySelector (".weather-image");
+const valueElement = document.querySelector (".temp-value p");
+const descElement = document.querySelector (".temp-desc p");
+const locationElement = document.querySelector (".temp-location p");
+const notifElement = document.querySelector (".notification");
+
+const weather = {};
+
+weather.temperature = {
+    unit: "Celsius"
+}
+
+const Fahreinheit = 273;
+
+const key = "e0f3620b70ce5624f312fc0522b2678d";
+
+if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+} else {
+    notifElement.style.display = "block";
+    notifElement.innerHTML = "<p> Unable to acccess</p>";
+}
+
+function setPosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
+
+function showError(error) {
+    notifElement.style.display = "block";
+    notifElement.innerHTML = `<p> ${error.message} </p>`;
+}
+
+function getWeather(latitude, longitude) {
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`
+
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data) {
+            weather.temperature.value = Math.floor(data.main.temp - Fahreinheit);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then (function() {
+            displayWeather();
+        })
+}
+
+function displayWeather() {
+    imageElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    valueElement.innerHTML = `${weather.temperature.value}°<span>F</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = ` Location: ${weather.city}, ${weather.country}`;
+}
